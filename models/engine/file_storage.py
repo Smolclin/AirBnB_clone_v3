fb1bf077955d384790dfeac3ddf6ba350db3e511
Smolclin <clinsmol10@gmail.com>
2024-06-09 11:20:57 -0700

    Fix code

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 Contains the FileStorage class
 """
 
+import models
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -11,6 +12,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from hashlib import md5
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -44,7 +46,9 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            if key == "password":
+                json_objects[key].decode()
+            json_objects[key] = self.__objects[key].to_dict(save_fs=1)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
@@ -68,3 +72,29 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """return the objects based on class name and id, or None if
+        not found"""
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (values.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        all_clas = classes.value()
+
+        if not cls:
+            count = 0
+            for clas in all_clas:
+                count += len(models.storageall(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
