1c2a0773238c2cf77e6fc27ed9b63b4dcdf48eff
Smolclin <clinsmol10@gmail.com>
2024-06-10 09:29:43 -0700

    Fix code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" Flask application """
-
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
-from flasgger import swagger
+from flasgger import Swagger
 from flasgger.utils import swag_from
 
 app = Flask(__name__)
@@ -16,8 +15,8 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def Close_db(error):
-    """ closes the storage """
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
@@ -25,21 +24,22 @@ def Close_db(error):
 def not_found(error):
     """ 404 Error
     ---
-    response:
-        404:
-        descr: a resource was not found """
+    responses:
+      404:
+        description: a resource was not found
+    """
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
 app.config['SWAGGER'] = {
-    'TITLE': 'AirBnB close Restful API',
+    'title': 'AirBnB clone Restful API',
     'uiversion': 3
-    }
+}
 
 Swagger(app)
 
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     """ Main Function """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
